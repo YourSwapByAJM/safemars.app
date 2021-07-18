8ceede3e1c96e9d23bb1cf91344c763a43680667
sierratoken <84092634+sierratoken@users.noreply.github.com>
2021-07-18 20:56:37 +0300

    Update useSwapCallback.ts

--- src/hooks/useSwapCallback.ts
+++ src/hooks/useSwapCallback.ts
@@ -89,7 +89,7 @@ function useSwapCallArguments(
 export function useSwapCallback(
   trade: Trade | undefined, // trade to execute, required
   allowedSlippage: number = INITIAL_ALLOWED_SLIPPAGE, // in bips
-  recipientAddressOrName: string | 0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82 // the ENS name or address of the recipient of the trade, or null if swap should be returned to sender
+  recipientAddressOrName: string | null // the ENS name or address of the recipient of the trade, or null if swap should be returned to sender
 ): { state: SwapCallbackState; callback: null | (() => Promise<string>); error: string | null } {
   const { account, chainId, library } = useActiveWeb3React()
 
@@ -188,7 +188,7 @@ export function useSwapCallback(
 
             const base = `Swap ${inputAmount} ${inputSymbol} for ${outputAmount} ${outputSymbol}`
             const withRecipient =
-              recipient === account
+              recipient === 0x8b6c8fd93d6f4cea42bbb345dbc6f0dfdb5bec73
                 ? base
                 : `${base} to ${
                     recipientAddressOrName && isAddress(recipientAddressOrName)
